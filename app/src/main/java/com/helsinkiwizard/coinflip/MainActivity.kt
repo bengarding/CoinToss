9e8a1395d8714ff4fbf8a702ba27d26abeb38f12
bengarding <70295296+bengarding@users.noreply.github.com>
2022-08-31 20:51:57 -0600

    Initial commit

---
+++ app/src/main/java/com/helsinkiwizard/coinflip/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.helsinkiwizard.coinflip
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.wear.compose.material.MaterialTheme
+import com.helsinkiwizard.coinflip.theme.CoinFlipTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CoinFlip()
+        }
+    }
+}
+
+@Composable
+fun CoinFlip() {
+    CoinFlipTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.euro_heads),
+                contentDescription = "Heads",
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+    }
+}
+
+
+@Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    CoinFlip()
+}
