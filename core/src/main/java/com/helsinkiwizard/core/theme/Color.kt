93968e3cda571a2e5ea6526bd69da6beee0458ee
bengarding <70295296+bengarding@users.noreply.github.com>
2024-03-05 21:24:10 -0700

    flip dark mode colors

--- core/src/main/java/com/helsinkiwizard/core/theme/Color.kt
+++ core/src/main/java/com/helsinkiwizard/core/theme/Color.kt
@@ -4,10 +4,4 @@ import androidx.compose.ui.graphics.Color
 
 val BlackTransparent = Color(0xB3000000)
 val LightBlue = Color(0xFF579BDB)
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-val Red400 = Color(0xFFCF6679)
 val WhiteTransparent = Color(0xAAFFFFFF)
-val TealSemiTransparent = Color(0xCC2CCFA6)
