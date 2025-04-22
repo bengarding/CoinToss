fce050778cc477135f6c7d7b047e1b5dc23566c7
bengarding <70295296+bengarding@users.noreply.github.com>
2025-04-21 21:11:43 -0600

    Use scaling lazy column

--- watch-app/src/main/java/com/helsinkiwizard/cointoss/ui/AboutScreen.kt
+++ watch-app/src/main/java/com/helsinkiwizard/cointoss/ui/AboutScreen.kt
@@ -1,22 +1,33 @@
 package com.helsinkiwizard.cointoss.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.gestures.animateScrollBy
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.input.rotary.onRotaryScrollEvent
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.wear.compose.foundation.ExperimentalWearFoundationApi
+import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
+import androidx.wear.compose.foundation.lazy.rememberScalingLazyListState
+import androidx.wear.compose.foundation.rememberActiveFocusRequester
 import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.PositionIndicator
+import androidx.wear.compose.material.Scaffold
 import androidx.wear.compose.material.Text
 import androidx.wear.tooling.preview.devices.WearDevices
 import com.helsinkiwizard.cointoss.BuildConfig
@@ -27,6 +38,7 @@ import com.helsinkiwizard.core.theme.Twelve
 import com.helsinkiwizard.core.theme.Twenty
 import com.helsinkiwizard.core.ui.composable.appIconPainterResource
 import com.helsinkiwizard.core.utils.getLastUpdatedDate
+import kotlinx.coroutines.launch
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
@@ -34,17 +46,37 @@ import java.util.Locale
 
 private val AppIconSize = 60.dp
 
+@OptIn(ExperimentalWearFoundationApi::class) // rememberActiveFocusRequester
 @Composable
 internal fun AboutScreen(
     dateUpdated: LocalDate = getLastUpdatedDate(LocalContext.current)
 ) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
+    val listState = rememberScalingLazyListState()
+    Scaffold(
+        positionIndicator = { PositionIndicator(scalingLazyListState = listState) }
     ) {
-        AppInfo(dateUpdated)
+        val focusRequester = rememberActiveFocusRequester()
+        val coroutineScope = rememberCoroutineScope()
+
+        ScalingLazyColumn(
+            state = listState,
+            contentPadding = PaddingValues(all = Twenty),
+            modifier = Modifier
+                .fillMaxSize()
+                .onRotaryScrollEvent {
+                    coroutineScope.launch {
+                        listState.scrollBy(it.verticalScrollPixels)
+                        listState.animateScrollBy(0f)
+                    }
+                    true
+                }
+                .focusRequester(focusRequester)
+                .focusable()
+        ) {
+            item {
+                AppInfo(dateUpdated)
+            }
+        }
     }
 }
 
@@ -54,9 +86,7 @@ private fun AppInfo(
 ) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(all = Twenty)
+        modifier = Modifier.fillMaxWidth()
     ) {
         Image(
             painter = appIconPainterResource(id = R.mipmap.ic_launcher_round),
