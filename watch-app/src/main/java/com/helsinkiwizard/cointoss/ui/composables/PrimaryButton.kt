b8e130549e3e5733d74f0320b21078197d1b7d1c
bengarding <70295296+bengarding@users.noreply.github.com>
2025-04-24 21:45:23 -0600

    Add button to about screen

--- watch-app/src/main/java/com/helsinkiwizard/cointoss/ui/composables/PrimaryButton.kt
+++ watch-app/src/main/java/com/helsinkiwizard/cointoss/ui/composables/PrimaryButton.kt
@@ -2,6 +2,7 @@ package com.helsinkiwizard.cointoss.ui.composables
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.wear.compose.material.Chip
 import androidx.wear.compose.material.Text
 
@@ -12,7 +13,12 @@ fun PrimaryButton(
     modifier: Modifier = Modifier
 ) {
     Chip(
-        label = { Text(text = text) },
+        label = {
+            Text(
+                text = text,
+                textAlign = TextAlign.Center
+            )
+        },
         onClick = onClick,
         modifier = modifier,
     )
