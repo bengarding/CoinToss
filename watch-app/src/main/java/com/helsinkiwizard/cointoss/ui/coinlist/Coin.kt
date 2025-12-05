a4eaddf90c2bce1fd2b7714016511b325d5eee1c
Ben Garding <70295296+bengarding@users.noreply.github.com>
2025-12-05 14:55:24 -0700

    Start and stop gesture detector using lifecycle state

--- watch-app/src/main/java/com/helsinkiwizard/cointoss/ui/coinlist/Coin.kt
+++ watch-app/src/main/java/com/helsinkiwizard/cointoss/ui/coinlist/Coin.kt
@@ -1,16 +1,13 @@
 package com.helsinkiwizard.cointoss.ui.coinlist
 
 import android.media.MediaPlayer
-import android.view.WindowManager
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -19,17 +16,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.input.rotary.onRotaryScrollEvent
+import androidx.lifecycle.compose.LifecycleResumeEffect
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.PagerState
 import com.helsinkiwizard.cointoss.R
 import com.helsinkiwizard.cointoss.ui.composables.Chevron
 import com.helsinkiwizard.cointoss.utils.FlipGestureDetector
-import com.helsinkiwizard.core.CoreConstants.VALUE_UNDEFINED
 import com.helsinkiwizard.core.coin.CoinAnimation
 import com.helsinkiwizard.core.coin.CoinType
 import com.helsinkiwizard.core.theme.LocalActivity
 import com.helsinkiwizard.core.ui.model.CustomCoinUiModel
-import kotlinx.coroutines.delay
 import kotlin.math.absoluteValue
 
 private const val SENSITIVITY_MULTIPLIER = 100
@@ -71,7 +67,6 @@ fun Coin(
         val soundEffect = remember { MediaPlayer.create(activity, R.raw.coin_toss) }
         var tossFromRotaryInput by remember { mutableStateOf(false) }
         var accumulatedDelta by remember { mutableFloatStateOf(0f) }
-        var secondsSinceLastFlip: Int by remember { mutableIntStateOf(VALUE_UNDEFINED) }
 
         val gestureDetector = remember {
             FlipGestureDetector(
@@ -89,32 +84,11 @@ fun Coin(
             focusRequester.requestFocus()
         }
 
-        DisposableEffect(tossFromWristFlip) {
-            if (tossFromWristFlip) {
-                gestureDetector.start()
-                activity.window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-            } else {
-                gestureDetector.stop()
-                activity.window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-            }
-            onDispose {
-                gestureDetector.stop()
-                activity.window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-            }
-        }
+        LifecycleResumeEffect(tossFromWristFlip) {
+            if (tossFromWristFlip) gestureDetector.start()
 
-        LaunchedEffect(secondsSinceLastFlip) {
-            when {
-                secondsSinceLastFlip == TEN_SECONDS -> {
-                    gestureDetector.stop()
-                    activity.window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-                    secondsSinceLastFlip = VALUE_UNDEFINED
-                }
-
-                tossFromWristFlip -> {
-                    delay(ONE_SECOND_MILLIS)
-                    secondsSinceLastFlip++
-                }
+            onPauseOrDispose {
+                gestureDetector.stop()
             }
         }
 
@@ -128,7 +102,6 @@ fun Coin(
                 if (playSound) soundEffect.start()
                 tossFromRotaryInput = false
                 onFlip()
-                if (tossFromWristFlip && secondsSinceLastFlip == VALUE_UNDEFINED) secondsSinceLastFlip = 0
             },
             modifier = Modifier
                 .fillMaxSize()
